441f9cc43d92600c8020417a0d4c78119b07576b
Thomas Musselman <tmusselman@gmail.com>
2022-08-02 10:30:36 -0600

    feat: cookbook creation date

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -1,9 +1,11 @@
 class CookBook
 
-  attr_reader :recipes
+  attr_reader :recipes,
+              :date
 
   def initialize
     @recipes = []
+    @date = Time.now.strftime("%m-%d-%Y")
   end
 
   def add_recipe(recipe)
