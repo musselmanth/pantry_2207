9448211b71ca298a0274f163ccc7f8dd29223a89
Thomas Musselman <tmusselman@gmail.com>
2022-08-02 11:13:45 -0600

    feat: cookbook summary method

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -22,4 +22,21 @@ class CookBook
     @recipes.max_by{ |recipe| recipe.total_calories }
   end
 
+  def summary
+    @recipes.map do |recipe|
+      {
+        name: recipe.name,
+        details: {
+          ingredients: recipe.caloricly_sorted_ingredients_req.map do |ingredient, qty|
+            {
+              ingredient: ingredient.name,
+              amount: "#{qty} #{ingredient.unit}"
+            }
+          end,
+          total_calories: recipe.total_calories
+        }
+      }
+    end
+  end
+
 end
