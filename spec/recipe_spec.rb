8ff0f2570710d97a6d8dd7f0279961bf7ebaeb09
Thomas Musselman <tmusselman@gmail.com>
2022-08-02 09:46:36 -0600

    test: recipe total_calories

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -5,7 +5,10 @@ require './lib/ingredient'
 RSpec.describe Recipe do
   let(:ingredient1){Ingredient.new({name: "Cheese", unit: "C", calories: 100})}
   let(:ingredient2){Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})}
+  let(:ingredient3{Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})}
+  let(:ingredient4{Ingredient.new({name: "Bun", unit: "g", calories: 75})}
   let(:recipe1){Recipe.new("Mac and Cheese")}
+  let(:recipe2){Recipe.new("Cheese Burger")}
 
   it 'exists and has a name' do
     expect(recipe1).to be_an(Recipe)
@@ -35,6 +38,17 @@ RSpec.describe Recipe do
     expected = [ingredient1, ingredient2]
 
     expect(recipe1.ingredients).to eq(expected)
+  end
+
+  it 'can calorize' do
+    recipe1.add_ingredient(ingredient2, 8)
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe2.add_ingredient(ingredient1, 2)
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 1)
+
+    expect(recipe1.total_calories).to eq(440)
+    expect(recipe2.total_calories).to eq(675)
   end
 
 end
