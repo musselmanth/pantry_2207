fcbd896e715ea0445cbd0e379b77e7fde244cebb
Thomas Musselman <tmusselman@gmail.com>
2022-08-02 10:00:19 -0600

    feat: cookbook ingredients and highest_calorie_meal methods

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -10,4 +10,14 @@ class CookBook
     @recipes << recipe
   end
 
+  def ingredients
+    @recipes.flat_map do |recipe| 
+      recipe.ingredients.map{ |ingredient| ingredient.name }
+    end.uniq
+  end
+
+  def highest_calorie_meal
+    @recipes.max_by{ |recipe| recipe.total_calories }
+  end
+
 end
